467b0dec0aebe405b7a5ee6cc6db71b7d98e1457
glabek94 <glab.dawid@gmail.com>
2017-12-14 18:26:54 +0100

    Day 13 with 1 day delay, but still 2 stars.

---
+++ Day13/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputFile = File.OpenText("../../input.txt");
+            string input = inputFile.ReadToEnd().Trim();
+
+            Dictionary<int, int> firewalls = new Dictionary<int, int>();
+            foreach (var line in input.Split('\n'))
+            {
+                var elems = line.Split(':');
+                int level = int.Parse(elems[0]);
+                int width = int.Parse(elems[1]);
+                firewalls.Add(level, width);
+            }
+
+            int severity = 0;
+            foreach (var firewall in firewalls)
+            {
+                int pos = countPosition(firewall.Value, firewall.Key);
+                if (pos == 0)
+                {
+                    severity += firewall.Value * firewall.Key;
+                }
+            }
+
+            Console.WriteLine(severity);
+
+            int delay = 0;
+            bool isGood = false;
+            while (!isGood)
+            {
+                isGood = true;
+                foreach (var firewall in firewalls)
+                {
+                    int pos = countPosition(firewall.Value, firewall.Key + delay);
+                    if (pos == 0)
+                    {
+                        delay++;
+                        isGood = false;
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine(delay);
+        }
+
+        static int countPosition(int width, int time)
+        {
+            int lengthOfCycle = 2 * (width - 1);
+            int position = time % lengthOfCycle;
+
+            if (position >= width)
+            {
+                return lengthOfCycle - position;
+            }
+            else
+            {
+                return position;
+            }
+        }
+    }
+}
