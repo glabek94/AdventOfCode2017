adacf893e69b04e99b313397256d541e9b704d7a
glabek94 <glab.dawid@gmail.com>
2017-12-23 21:37:06 +0100

    First part of day 23.

---
+++ Day23/AssemblerExecutor.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day23
+{
+    public class AssemblerExecutor
+    {
+        List<Instruction> instructions;
+        Dictionary<char, long> registers;
+        int insPtr;
+
+        int howManyMul = 0;
+
+        public AssemblerExecutor()
+        {
+            instructions = new List<Instruction>();
+            registers = new Dictionary<char, long>();
+            AddRegister('a');
+            insPtr = 0;
+        }
+
+        private void AddRegister(char name)
+        {
+            registers.Add(name, 0);
+        }
+
+        public int Execute()
+        {
+            while (insPtr < instructions.Count)
+            {
+                ExecuteInstruction(instructions[insPtr]);
+            }
+
+            return howManyMul;
+        }
+
+        public void AddInstruction(Instruction toAdd)
+        {
+            instructions.Add(toAdd);
+        }
+
+        private void ExecuteInstruction(Instruction toExecute)
+        {
+            long firstOpValue = 0;
+            long secondOpValue = 0;
+
+            if (!int.TryParse(toExecute.first.ToString(), out int tmp))
+            {
+                if (!registers.ContainsKey(toExecute.first))
+                    AddRegister(toExecute.first);
+
+                firstOpValue = registers[toExecute.first];
+            }
+            else
+            {
+                firstOpValue = tmp;
+            }
+
+            if (toExecute.second.Length > 0)
+            {
+                if (!int.TryParse(toExecute.second, out tmp))
+                {
+                    if (!registers.ContainsKey(toExecute.second[0]))
+                        AddRegister(toExecute.second[0]);
+
+                    if (toExecute.second.Length > 0)
+                        secondOpValue = registers[toExecute.second[0]];
+                }
+                else
+                {
+                    secondOpValue = tmp;
+                }
+            }
+
+            switch (toExecute.name)
+            {
+                case Instruction.instructionType.set:
+                    registers[toExecute.first] = secondOpValue;
+                    insPtr++;
+                    break;
+                case Instruction.instructionType.mul:
+                    howManyMul++;
+                    registers[toExecute.first] = firstOpValue * secondOpValue;
+                    insPtr++;
+                    break;
+                case Instruction.instructionType.jnz:
+                    if (firstOpValue != 0)
+                    {
+                        insPtr += (int)secondOpValue;
+                    }
+                    else
+                    {
+                        insPtr++;
+                    }
+                    break;
+                case Instruction.instructionType.sub:
+                    registers[toExecute.first] = firstOpValue - secondOpValue;
+                    insPtr++;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public struct Instruction
+        {
+            public instructionType name { get; }
+            public char first { get; }
+            public string second { get; }
+
+            public Instruction(instructionType name, char first, string second = "")
+            {
+                this.name = name;
+                this.first = first;
+                this.second = second;
+            }
+
+            public enum instructionType { set, mul, sub, jnz };
+        }
+    }
+}
---
+++ Day23/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputFile = File.OpenText("../../input.txt");
+            string input = inputFile.ReadToEnd().Trim();
+
+            AssemblerExecutor executor = new AssemblerExecutor();
+
+            foreach (var line in input.Split('\n'))
+            {
+                var currentLine = line.Trim();
+                AssemblerExecutor.Instruction.instructionType instructionType = AssemblerExecutor.Instruction.instructionType.set;
+                char firstOperand;
+                string secondOperand = "";
+
+                var splitted = currentLine.Split(' ');
+                switch (splitted[0])
+                {
+                    case "set":
+                        instructionType = AssemblerExecutor.Instruction.instructionType.set;
+                        break;
+                    case "mul":
+                        instructionType = AssemblerExecutor.Instruction.instructionType.mul;
+                        break;
+                    case "jnz":
+                        instructionType = AssemblerExecutor.Instruction.instructionType.jnz;
+                        break;
+                    case "sub":
+                        instructionType = AssemblerExecutor.Instruction.instructionType.sub;
+                        break;
+                }
+                firstOperand = splitted[1][0];
+                if (splitted.Length > 2)
+                {
+                    secondOperand = splitted[2];
+                }
+
+                executor.AddInstruction(new AssemblerExecutor.Instruction(instructionType, firstOperand, secondOperand));
+            }
+
+            Console.WriteLine(executor.Execute());
+        }
+    }
+}
